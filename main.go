0c23769ccbf7e6b05818f9a9bf4534a37c6286fb
Owen Rees <owenrees82@gmail.com>
2023-10-04 11:51:06 +0200

    cache header

--- main.go
+++ main.go
@@ -17,16 +17,19 @@ const defaultPort = "8080"
 
 func main() {
 	dist, _ := fs.Sub(client, "client/dist")
-
-	http.Handle(
-		"/", http.FileServer(http.FS(dist)),
-	)
-
 	port := os.Getenv("PORT")
 
 	if port == "" {
 		port = defaultPort
 	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// set the cache header
+		w.Header().Set("Cache-Control", "public, max-age=604800")
+
+		// serve the file
+		http.FileServer(http.FS(dist)).ServeHTTP(w, r)
+	})
 
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
