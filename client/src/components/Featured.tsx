0e82797f7e56467b50f2e1c698c1af5cdda86929
Owen Rees <owenrees82@gmail.com>
2023-11-16 22:01:12 +0100

    chess-pdf added

--- client/src/components/Featured.tsx
+++ client/src/components/Featured.tsx
@@ -1,8 +1,11 @@
 import echecsfrance from "../assets/images/portfolio/images/echecsfrance.png";
 import plantIDBot from "../assets/images/portfolio/images/plantIDBot.png";
+import chessPDF from "../assets/images/portfolio/images/chessPDF.png";
 import echecsfranceThumbnail from "../assets/images/portfolio/thumbnails/echecsfrance.webp";
 import plantIDBotThumbnail from "../assets/images/portfolio/thumbnails/plantIDBot.webp";
-import upRightArrow from "../assets/svg/up-right-arrow.svg";
+import chessPDFThumbnail from "../assets/images/portfolio/thumbnails/chessPDF.webp";
+
+import Project from "./Project.tsx";
 
 function Featured() {
   return (
@@ -12,95 +15,37 @@ function Featured() {
         <h3 className="section-subtitle">Featured Projects</h3>
       </div>
       <div className="featured-projects">
-        <div className="project">
-          <a href={echecsfrance} target="_blank">
-            <img
-              src={echecsfranceThumbnail}
-              alt="Echecs France"
-              width="600px"
-              height="auto"
-            />
-          </a>
-          <div>
-            <h4 className="featured-title">Echecs France</h4>
-            <p className="featured-text">
-              A visual representation of FFE chess tournaments in France
-            </p>
-            <div className="featured-buttons">
-              <a
-                className="featured-live-button"
-                href="https://echecsfrance.com"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Live
-                <img
-                  src={upRightArrow}
-                  alt="up right arrow"
-                  className="button-arrow"
-                />
-              </a>
-              <a
-                className="featured-source-button"
-                href="https://github.com/TheRealOwenRees/echecsfrance"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Source
-                <img
-                  src={upRightArrow}
-                  alt="up right arrow"
-                  className="button-arrow"
-                />
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="project">
-          <a href={plantIDBot} target="_blank">
-            <img
-              src={plantIDBotThumbnail}
-              alt="Echecs France"
-              className="project-image"
-              width="600px"
-              height="auto"
-            />
-          </a>
-          <div>
-            <h4 className="featured-title">Plant ID Bot</h4>
-            <p className="featured-text">
-              A Discord bot that identifies plants from images of their organs
-            </p>
-            <div className="featured-buttons">
-              <a
-                className="featured-live-button"
-                href="https://discord.com/oauth2/authorize?client_id=948227126094598204&permissions=19520&scope=bot"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Invite
-                <img
-                  src={upRightArrow}
-                  alt="up right arrow"
-                  className="button-arrow"
-                />
-              </a>
-              <a
-                className="featured-source-button"
-                href="https://github.com/TheRealOwenRees/plantID_discordbot"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Source
-                <img
-                  src={upRightArrow}
-                  alt="up right arrow"
-                  className="button-arrow"
-                />
-              </a>
-            </div>
-          </div>
-        </div>
+        <Project
+          image={chessPDF}
+          thumbnail={chessPDFThumbnail}
+          title={"Chess PDF"}
+          text={"A PDF generator for chess games, written in TypeScript"}
+          liveButtonText={"Live"}
+          url={"https://chess-pdf.vercel.app/"}
+          github={"https://github.com/TheRealOwenRees/chess-pdf"}
+        />
+        <Project
+          image={echecsfrance}
+          thumbnail={echecsfranceThumbnail}
+          title={"Echecs France"}
+          text={"A visual representation of FFE chess tournaments in France"}
+          liveButtonText={"Live"}
+          url={"https://echecsfrance.com"}
+          github={"https://github.com/TheRealOwenRees/echecsfrance"}
+        />
+        <Project
+          image={plantIDBot}
+          thumbnail={plantIDBotThumbnail}
+          title={"Plant ID Bot"}
+          text={
+            "A Discord bot that identifies plants from images of their organs"
+          }
+          liveButtonText={"Invite"}
+          url={
+            "https://discord.com/oauth2/authorize?client_id=948227126094598204&permissions=19520&scope=bot"
+          }
+          github={"https://github.com/TheRealOwenRees/plantID_discordbot"}
+        />
       </div>
     </section>
   );
---
+++ client/types.ts
@@ -0,0 +1,9 @@
+export interface ProjectProps {
+  image: string;
+  thumbnail: string;
+  title: string;
+  text: string;
+  liveButtonText: string;
+  url: string;
+  github: string;
+}
