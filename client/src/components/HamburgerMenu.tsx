3deeb373d498a1c235ac6530a1691491997b7223
Owen Rees <owenrees82@gmail.com>
2023-11-19 22:02:02 +0100

    initial hamburger menu

---
+++ client/src/components/HamburgerMenu.tsx
@@ -0,0 +1,57 @@
+import { HamburgerProps } from "../../types.ts";
+import { LuGithub, LuLinkedin } from "react-icons/lu";
+
+const HamburgerMenu = ({ hamburgerOpen, setHamburgerOpen }: HamburgerProps) => {
+  const hamburgerMenuClass = hamburgerOpen ? "hamburger-menu-open" : "";
+
+  const handleCloseHamburgerMenu = () => {
+    setHamburgerOpen(false);
+  };
+
+  return (
+    <div
+      className={`hamburger-menu ${hamburgerMenuClass}`}
+      onClick={handleCloseHamburgerMenu}
+    >
+      <ul className="">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#blog">Blog</a>
+        </li>
+        <div className="icons">
+          <li className="icon">
+            <a
+              className="icon-link"
+              href="https://github.com/TheRealOwenRees"
+              target="_blank"
+              aria-label="GitHub"
+            >
+              <LuGithub />
+            </a>
+          </li>
+
+          <li className="icon">
+            <a
+              className="icon-link"
+              href="https://www.linkedin.com/in/therealowenrees/"
+              target="_blank"
+              aria-label="LinkedIn"
+            >
+              <LuLinkedin />
+            </a>
+          </li>
+        </div>
+      </ul>
+    </div>
+  );
+};
+
+export default HamburgerMenu;
--- client/types.ts
+++ client/types.ts
@@ -1,3 +1,5 @@
+import { Dispatch, SetStateAction } from "react";
+
 export interface ProjectProps {
   image: string;
   thumbnail: string;
@@ -12,4 +14,9 @@ export interface BlogPostProps {
   image: string;
   title: string;
   url: string;
+}
+
+export interface HamburgerProps {
+  hamburgerOpen: boolean;
+  setHamburgerOpen: Dispatch<SetStateAction<boolean>>;
 }
